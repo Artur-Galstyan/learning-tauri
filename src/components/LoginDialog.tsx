7e2b1991045cedb32eda797bda1888bb25fc68f7
Artur Galstyan <galstyan.artu@gmail.com>
2025-12-14 21:59:23 +0100

    more login stuff

--- src/components/LoginDialog.tsx
+++ src/components/LoginDialog.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { LOGIN_DIALOG_ID } from "../lib/notifications";
 
 function LoginDialog() {
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  function validatePassword(event: React.ChangeEvent<HTMLInputElement>) {
+    const password = event.target.value;
+    if (!password) return false;
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    const validity = regex.test(password);
+    setIsValidPassword(validity);
+    return validity;
+  }
+
   return (
     <>
       <dialog id={LOGIN_DIALOG_ID} className="modal">
@@ -8,7 +20,7 @@ function LoginDialog() {
           <h3 className="text-lg font-bold">Hello!</h3>
           <form>
             <input
-              className="input validator"
+              className="input"
               type="email"
               required
               placeholder="mail@site.com"
@@ -17,22 +29,23 @@ function LoginDialog() {
 
             <input
               type="password"
-              className="input validator"
+              className="input"
               required
+              onChange={validatePassword}
               placeholder="Password"
-              minlength="8"
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
               title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
             />
-            <p className="validator-hint">
-              Must be more than 8 characters, including
-              <br />
-              At least one number
-              <br />
-              At least one lowercase letter
-              <br />
-              At least one uppercase letter
-            </p>
+            {!isValidPassword && (
+              <p className="text-error">
+                Must be more than 8 characters, including
+                <br />
+                At least one number
+                <br />
+                At least one lowercase letter
+                <br />
+                At least one uppercase letter
+              </p>
+            )}
             <button className="btn" type="button">
               Login
             </button>
